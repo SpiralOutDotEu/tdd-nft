2aa8f35cdd36800dc2624814f5be29e13ca14e77
Jumaru <spiralout.eu@gmail.com>
2023-05-26 17:38:09 +0300

    It supports ERC721 interface

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ERC721Token {}
+contract ERC721Token is ERC721 {
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC721(name_, symbol_) {}
+}
--- test/ERC721Token.ts
+++ test/ERC721Token.ts
@@ -1,10 +1,25 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
+import { faker } from '@faker-js/faker';
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe('ERC721Token', function () {
-    it("Should be deployed", async function () {
+    async function deployERC721TokenFixture() {
         const Token = await ethers.getContractFactory('ERC721Token');
-        await Token.deploy();
+        const name = faker.string.alphanumeric({ length: { min: 5, max: 25 } });
+        const symbol = faker.string.alphanumeric({ length: { min: 3, max: 7 } });
+        const token = await Token.deploy(name, symbol);
+        return { token };
+    }
+    it("Should be deployed", async function () {
+        const { } = await loadFixture(deployERC721TokenFixture);
         expect(true);
     })
+    it('should support ERC721 interface', async function () {
+        const { token } = await loadFixture(deployERC721TokenFixture);
+        const supportsInterface = await token.supportsInterface(
+            '0x80ac58cd' // ERC721 interface ID
+        );
+        expect(supportsInterface).to.equal(true);
+    });
 })
