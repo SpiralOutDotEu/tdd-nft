dda1c5dc5450178eab63edb56013456545d66b60
Jumaru <spiralout.eu@gmail.com>
2023-06-01 11:09:27 +0300

    It handles ownership

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721Token is ERC721 {
+contract ERC721Token is ERC721, Ownable {
     constructor(
         string memory name_,
         string memory symbol_
--- test/ERC721Token.ts
+++ test/ERC721Token.ts
@@ -9,17 +9,36 @@ describe('ERC721Token', function () {
         const name = faker.string.alphanumeric({ length: { min: 5, max: 25 } });
         const symbol = faker.string.alphanumeric({ length: { min: 3, max: 7 } });
         const token = await Token.deploy(name, symbol);
-        return { token };
+        const [deployer] = await ethers.getSigners();
+        return { token, name, symbol, deployer };
     }
-    it("Should be deployed", async function () {
-        const { } = await loadFixture(deployERC721TokenFixture);
-        expect(true);
-    })
+    it("Should be deployed with owner, name and symbol", async function () {
+        const { token, name, symbol, deployer } = await loadFixture(deployERC721TokenFixture);
+        const owner = await token.owner();
+        const tokenName = await token.name();
+        const tokenSymbol = await token.symbol();
+        expect(owner).to.equal(deployer.address);
+        expect(tokenName).to.equal(name);
+        expect(tokenSymbol).to.equal(symbol);
+    });
     it('should support ERC721 interface', async function () {
         const { token } = await loadFixture(deployERC721TokenFixture);
         const supportsInterface = await token.supportsInterface(
             '0x80ac58cd' // ERC721 interface ID
         );
         expect(supportsInterface).to.equal(true);
+    });
+    it("Should transfer ownership", async function () {
+        const { token } = await loadFixture(deployERC721TokenFixture);
+        const [owner, newOwner] = await ethers.getSigners();
+        await token.transferOwnership(newOwner.address);
+        const newContractOwner = await token.owner();
+        expect(newContractOwner).to.equal(newOwner.address);
+    });
+    it("Should renounce ownership", async function () {
+        const { token } = await loadFixture(deployERC721TokenFixture);
+        await token.renounceOwnership();
+        const newContractOwner = await token.owner();
+        expect(newContractOwner).to.equal(ethers.constants.AddressZero);
     });
 })
